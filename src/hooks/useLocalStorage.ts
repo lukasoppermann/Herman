a39b0456d1b272d7e09a0d5413a79b2075ba046b
Lukas Oppermann <lukasoppermann@github.com>
2025-01-15 08:24:23 +0100

    add draggable list

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+  // Get the initial value from local storage or use the provided initial value
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  // Update local storage whenever the stored value changes
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      console.log("setValue", key, value)
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage;
